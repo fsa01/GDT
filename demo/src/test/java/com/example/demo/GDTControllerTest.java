02deb665368da830ab8175bbb065c033defc887c
fsa01 <fabrizio.antonino.simone@hotmail.it>
2024-02-22 17:19:13 +0100

    aggiunto controller e test

---
+++ demo/src/test/java/com/example/demo/GDTControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controller.Controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(Controller.class)
+public class GDTControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+}
+
